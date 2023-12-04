dcc8c98d621447fc0e815bc2b5de2c9acd835b2e
Suman D R <106007784+Suman-D-R@users.noreply.github.com>
2023-12-04 12:54:22 +0530

    [suman] Password: hashing is added

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import * as userController from '../controllers/user.controller';
 import { newUserValidator } from '../validators/user.validator';
-import { userAuth } from '../middlewares/auth.middleware';
 
 const router = express.Router();
-
-//route to get all users
-router.get('', userController.getAllUsers);
 
 //route to create a new user
 router.post('', newUserValidator, userController.userRegistration);
@@ -14,14 +10,5 @@ router.post('', newUserValidator, userController.userRegistration);
 //route to login user
 router.post('/login', userController.loginUser);
 
-
-//route to get a single user by their user id
-router.get('/:email', userController.getUser);
-
-//route to update a single user by their user id
-router.put('/:_id',newUserValidator, userController.updateUser);
-
-//route to delete a single user by their user id
-router.delete('/:_id',newUserValidator, userController.deleteUser);
 
 export default router;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,64 +1,33 @@
 import User from '../models/user.model';
+import bcrypt from 'bcrypt';
 
-//get all users
-export const getAllUsers = async () => {
-  const data = await User.find();
-  return data;
-};
 
 //login
 
 export const loginUser = async (userDetails) => {
-  const data = await User.findOne({email: userDetails.email } );
-  if (!data) {
+  const user = await User.findOne({email: userDetails.email } );
+  if (!user) {
     throw new Error('User not found');
   }
 
-  if (userDetails.email === data.email && userDetails.password !== data.password) {
+  const passwordMatch = await bcrypt.compare(userDetails.password, user.password);
+
+  if (!passwordMatch) {
     throw new Error('Password not matched');
   }
-
-  if (userDetails.email === data.email && userDetails.password === data.password) {
-    return data;
-  }
-  return null
+  return user;
 };
 
 //create new user
 export const userRegistration = async (body) => {
   const email = await User.findOne({email:body.email})
-  const data = await User.create(body)
   if(email){
     throw new Error('user alrady have an account');
   }
+  const hashedPassword = await bcrypt.hash(body.password, 10);
+  body.password = hashedPassword;
+  console.log("body :>",body)
+  const data = await User.create(body)
   return data
 };
 
-//update single user
-export const updateUser = async (_id, body) => {
-  const data = await User.findByIdAndUpdate(
-    {
-      _id
-    },
-    body,
-    {
-      new: true
-    }
-  );
-  return data;
-};
-
-//delete single user
-export const deleteUser = async (id) => {
-  await User.findByIdAndDelete(id);
-  return '';
-};
-
-//get single user
-export const getUser = async (email) => {
-  const data = await User.findOne({email:email});
-  if(!data){
-    throw new Error('user not found')
-  }
-  return data;
-};
